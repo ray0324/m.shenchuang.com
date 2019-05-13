87deb2534701279ab5cf72e8a3b2b8cbf3d94dc1
ray0324 <ray0324@foxmail.com>
2019-05-14 01:39:30 +0800

    update

--- gulpfile.js
+++ gulpfile.js
@@ -1,53 +1,58 @@
-const gulp = require('gulp');
-const less = require('gulp-less');
-const minifycss = require('gulp-minify-css');
-const autoprefixer = require('gulp-autoprefixer');
-const rename = require('gulp-rename');
-const uglify = require('gulp-uglify');
-const del = require('del');
-const browsersync = require('browser-sync');
-
+const gulp = require("gulp");
+const less = require("gulp-less");
+const minifycss = require("gulp-minify-css");
+const autoprefixer = require("gulp-autoprefixer");
+const rename = require("gulp-rename");
+const uglify = require("gulp-uglify");
+const del = require("del");
+const browsersync = require("browser-sync");
 
 function clean() {
-  return del(['dist/**/*']);
+  return del(["dist/**/*"]);
 }
 
 function css() {
-  return gulp.src(['./src/less/*.less', '!./src/less/var.less'])
+  return gulp
+    .src(["./src/less/*.less", "!./src/less/var.less"])
     .pipe(less())
-    .pipe(autoprefixer({ browsers: ['chrome>40','ie>=8','firefox>25'], cascade: false }))
-    .pipe(rename({ suffix: '.min' }))
+    .pipe(
+      autoprefixer({
+        browsers: ["chrome>40", "ie>=8", "firefox>25"],
+        cascade: false
+      })
+    )
+    .pipe(rename({ suffix: ".min" }))
     .pipe(minifycss())
-    .pipe(gulp.dest('./dist/css/'))
+    .pipe(gulp.dest("./dist/css/"));
 }
 
 function js() {
-  return gulp.src(['./src/js/**/*.js'])
-    .pipe(uglify({
-      mangle: { reserved: ['require', 'exports', 'module', '$'] }
-    }))
-    .pipe(gulp.dest('./dist/js/'))
+  return gulp
+    .src(["./src/js/**/*.js"])
+    .pipe(
+      uglify({
+        mangle: { reserved: ["require", "exports", "module", "$"] }
+      })
+    )
+    .pipe(gulp.dest("./dist/js/"));
 }
 
 function html() {
-  return gulp.src('./src/*.html')
-    .pipe(gulp.dest('./dist/'))
+  return gulp.src("./src/*.html").pipe(gulp.dest("./dist/"));
   // .pipe(browsersync.reload({ stream: true }))
 }
 
 function images() {
-  return gulp.src(['./src/images/**/*'])
-    .pipe(gulp.dest('./dist/images/'))
+  return gulp.src(["./src/images/**/*"]).pipe(gulp.dest("./dist/images/"));
 }
 
 function vendor() {
-  return gulp.src(['./src/vendor/**/*'])
-    .pipe(gulp.dest('./dist/vendor/'))
+  return gulp.src(["./src/vendor/**/*"]).pipe(gulp.dest("./dist/vendor/"));
 }
 
 function reload(cb) {
   browsersync.reload();
-  cb()
+  cb();
 }
 
 function serve(cb) {
@@ -59,11 +64,15 @@ function serve(cb) {
       directory: true
     }
   });
-  gulp.watch('src/less/**/*.less', css);
-  gulp.watch('src/*.html', html);
-  gulp.watch('src/js/*.js', js);
-  gulp.watch('src/images/**/*', images);
-  gulp.watch(['*.html', 'css/*.css', 'js/**/*.js', 'images/**/*'], { cwd: 'dist' }, reload);
+  gulp.watch("src/less/**/*.less", css);
+  gulp.watch("src/*.html", html);
+  gulp.watch("src/js/*.js", js);
+  gulp.watch("src/images/**/*", images);
+  gulp.watch(
+    ["*.html", "css/*.css", "js/**/*.js", "images/**/*"],
+    { cwd: "dist" },
+    reload
+  );
   cb();
 }
 
@@ -71,6 +80,13 @@ function serve(cb) {
 //   gulp.series(gulp.parallel(html, images, vendor, css, js));
 // }
 
-exports.watch = gulp.series(clean, gulp.parallel(html, images, vendor, css, js), serve);
-exports.build = gulp.series(clean, gulp.parallel(html, images, vendor, css, js));
+exports.watch = gulp.series(
+  clean,
+  gulp.parallel(html, images, vendor, css, js),
+  serve
+);
+exports.build = gulp.series(
+  clean,
+  gulp.parallel(html, images, vendor, css, js)
+);
 exports.clean = clean;
